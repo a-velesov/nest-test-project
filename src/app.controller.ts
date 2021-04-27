d13289b34d6bf3102353d4f2ddaa778ad2152757
Alexsandr <a.velesov@devkeeper.ru>
2021-04-27 18:50:42 +0300

    base setiing service & controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService) {
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  getUsers() {
+    return this.appService.getUsers();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+
+async function start() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Сервер запущен на ${PORT}`));
 }
-bootstrap();
+start();
