1e75a20c8de340800766e14519b3ec439cb77a0a
Alexsandr <a.velesov@devkeeper.ru>
2021-05-11 19:54:12 +0300

    add & setting cross env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'nest-test-project',
+      host: process.env.POSTGRES_HOST,
+      port:  Number(process.env.POSTGRES_PORT),
+      username:  process.env.POSTGRES_USER,
+      password:  process.env.POSTGRES_PASSWORD,
+      database:  process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
