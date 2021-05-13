961dbce794b114af9a046f70b0fab3c7d0e0ec3e
Alexsandr <a.velesov@devkeeper.ru>
2021-05-13 10:33:15 +0300

    add users controller; refactor

--- src/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
-
-@Controller('/api')
-export class AppController {
-  constructor(private appService: AppService) {
-  }
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { User } from "./users/users.model";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
