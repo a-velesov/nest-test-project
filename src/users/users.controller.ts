17b35069e1f53c6de491255d1336e24877bc96b4
Alexsandr <a.velesov@devkeeper.ru>
2021-05-20 11:18:33 +0300

    added roles guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { UsersService } from "./users.service";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { User } from "./users.model";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Roles } from "src/auth/roles-auth.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
 
 @ApiTags('Пользователи')
 @Controller("users")
@@ -21,7 +23,8 @@ export class UsersController {
 
   @ApiOperation({ summary: "Получение всех пользователей" })
   @ApiResponse({ status: 200, type: [User] })
-  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
